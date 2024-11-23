9bae0f8aeb396b9f3a8f343d03a618ed6bb1b035
BooleanEnjoyer <259745@student.pwr.edu.pl>
2024-11-24 00:07:29 +0100

    Prediction mc docker image setup, fixing logback

--- api-gateway/src/main/java/com/example/microservice/apigateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/example/microservice/apigateway/config/GatewayConfig.java
@@ -1,8 +1,10 @@
 package com.example.microservice.apigateway.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.gateway.filter.factory.SpringCloudCircuitBreakerFilterFactory;
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +27,11 @@ public class GatewayConfig {
     private static final String PREDICTION_PATH = "/predictor/**";
     private static final String PREDICTION_MC_URI = "http://localhost:5000";
 
+
+    //DOCKER URI-S
+    private static final String PREDICTION_MC_DOCKER_ID = "prediction-mc-docker";
+    private static final String PREDICTION_MC_DOCKER_URI = "http://prediction-mc-container:5000";
+
     //DISCOVERY SERVER
     private static final String DISCOVERY_SERVER_ID = "discovery-server";
     private static final String DISCOVERY_SERVER_STATIC_ID = "discovery-server-static";
@@ -32,33 +39,42 @@ public class GatewayConfig {
     private static final String DISCOVERY_SERVER_STATIC_PATH = "/eureka";
     private static final String DISCOVERY_SERVER_URI = "http://localhost:8761";
 
+
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(AUTH_MC_ID, r -> r.path(AUTH_PATH)
-                        .filters(f ->
-                                f
-                                        .circuitBreaker(config -> config
-                                                .setName("authCircuitBreaker")
-                                                .setFallbackUri("forward:/fallback"))
-                                        .filter(securityConfig))
-                        .uri(AUTH_MC_URI))
-                .route(AUTH_MC_TOKEN_ID, r -> r.path(AUTH_TOKEN_PATH)
-                        .filters(f ->
-                                f
-                                        .circuitBreaker(config -> config
-                                                .setName("authCircuitBreaker")
-                                                .setFallbackUri("forward:/fallback"))
-                                        .filter(securityConfig))
-                        .uri(AUTH_MC_URI))
-                .route(PREDICTION_MC_ID, r -> r.path(PREDICTION_PATH)
-                        .filters(f ->
-                                f
-                                        .circuitBreaker(config -> config
-                                                .setName("authCircuitBreaker")
-                                                .setFallbackUri("forward:/fallback"))
-                                        .filter(securityConfig))
-                        .uri(PREDICTION_MC_URI))
+//                .route(AUTH_MC_ID, r -> r.path(AUTH_PATH)
+//                        .filters(f ->
+//                                f
+//                                        .circuitBreaker(config -> config
+//                                                .setName("authCircuitBreaker")
+//                                                .setFallbackUri("forward:/fallback"))
+//                                        .filter(securityConfig))
+//                        .uri(AUTH_MC_URI))
+//                .route(AUTH_MC_TOKEN_ID, r -> r.path(AUTH_TOKEN_PATH)
+//                        .filters(f ->
+//                                f
+//                                        .circuitBreaker(config -> config
+//                                                .setName("authCircuitBreaker")
+//                                                .setFallbackUri("forward:/fallback"))
+//                                        .filter(securityConfig))
+//                        .uri(AUTH_MC_URI))
+//                .route(PREDICTION_MC_ID, r -> r.path(PREDICTION_PATH)
+//                        .filters(f ->
+//                                f
+//                                        .circuitBreaker(config -> config
+//                                                .setName("authCircuitBreaker")
+//                                                .setFallbackUri("forward:/fallback"))
+//                                        .filter(securityConfig))
+//                        .uri(PREDICTION_MC_URI))
+
+                .route(AUTH_MC_ID, r -> configureRoute(r, AUTH_PATH, AUTH_MC_URI))
+                .route(AUTH_MC_TOKEN_ID, r -> configureRoute(r, AUTH_TOKEN_PATH, AUTH_MC_URI))
+//                .route(PREDICTION_MC_ID, r -> configureRoute(r, PREDICTION_PATH, PREDICTION_MC_URI))
+
+                //Docker URI/s TODO change all aplications for profiles
+                .route(PREDICTION_MC_DOCKER_ID, r -> configureRoute(r, PREDICTION_PATH, PREDICTION_MC_DOCKER_URI))
+
 //                .route(DISCOVERY_SERVER_ID, r -> r.path(DISCOVERY_SERVER_PATH)
 //                        .filters(f -> f.filter(securityConfig))
 //                        .uri(DISCOVERY_SERVER_URI))
@@ -66,5 +82,15 @@ public class GatewayConfig {
 //                        .filters(f -> f.filter(securityConfig))
 //                        .uri(DISCOVERY_SERVER_URI))
                 .build();
+    }
+
+    private Buildable<Route> configureRoute(PredicateSpec r, String path, String uri) {
+        return r.path(path)
+                .filters(f -> f
+                        .circuitBreaker(config -> config
+                                .setName("authCircuitBreaker")
+                                .setFallbackUri("forward:/fallback"))
+                        .filter(securityConfig))
+                .uri(uri);
     }
 }
